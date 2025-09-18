f9e4feaafb0d53e8f2ac970e0f9462f95133f41d
Samuel Easton (RIT Student) <sge3418@rit.edu>
2025-09-18 13:13:20 -0400

    Revert "refixed it"
    This reverts commit 52683372205de429d1786c4433cd85cb8ff0f214.

--- Load/WaveDirector.cs
+++ Load/WaveDirector.cs
@@ -1,6 +1,36 @@
 using Godot;
 using System;
 
-public partial class WaveDirector : Node
+public partial class WaveDirector : Node2D
 {
+	[Export] public PackedScene EnemyScene;
+	[Export] public NodePath MapEnemiesPath;
+	[Export] public NodePath Path2DPath;
+	[Export] public float SpawnEvery = 1.0f;    // Seconds between spawns
+
+	private float _timer;
+	private Node2D _enemiesRoot;
+	private Path2D _path2D;
+	
+	public override void _Ready()
+	{
+	   _enemiesRoot = GetNode<Node2D>(MapEnemiesPath);
+	   _path2D = GetNode<Path2D>(Path2DPath);
+	}
+
+	public override void _Process(double delta)
+	{
+		_timer += (float)delta;
+		if (_timer < SpawnEvery) return;
+		_timer = 0f;
+
+		if (EnemyScene == null) return;
+
+		// Spawn enemy on the Enemies node
+		 var enemy = (Enemy)EnemyScene.Instantiate();
+		_enemiesRoot.AddChild(enemy);
+
+		enemy.SetPath(_path2D);
+
+	}
 }
--- Scripts/Gameplay/Enemies/Enemy.cs
+++ Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,6 +1,26 @@
 using Godot;
 using System;
 
-public partial class Enemy : Node
+public partial class Enemy : Node2D
 {
+	[Export] public float MaxHp = 30f;
+	public float HP;
+
+	public override void _Ready()
+	{
+		HP = MaxHp;
+	}
+
+	public void TakeDamage(float dmg)
+	{
+		HP -= dmg;
+		if (HP <= 0f)
+			QueueFree();
+	}
+	
+	public void SetPath(Path2D path)
+	{
+		var follower = GetNode<PathFollower>("PathFollower");
+		follower.SetPath(path);
+	}
 }
