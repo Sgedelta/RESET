a8f4cdf121866223f26f59db52ce48ce2bd92b39
Samuel Easton (RIT Student) <sge3418@rit.edu>
2025-09-23 13:27:14 -0400

    added gitignore change, added TowerStats enum

--- Scripts/Gameplay/Towers/Tower.cs
+++ Scripts/Gameplay/Towers/Tower.cs
@@ -3,8 +3,13 @@ using System;
 
 public partial class Tower : Node2D
 {
+	//==========STATS============
+
+
+
 	[Export] public float BaseDamage = 5f;
 	[Export] public float BaseFireRate = 1.5f; // shots per second
+
 
 	public TargetingComponent Targeting { get; private set; }
 	public ShooterComponent Shooter { get; private set; }
@@ -15,4 +20,33 @@ public partial class Tower : Node2D
 		Shooter   = GetNode<ShooterComponent>("ShooterComponent");
 		Shooter.SetStats(BaseFireRate, BaseDamage);
 	}
+}
+
+
+public struct TowerStats {
+
+	//Basic Stats
+	public int AspectSlots = 3;
+	public float FireRate = 1.5f; //fire rate in shots per second
+	public float Damage = 5f;
+	public float Range = 500; //range in pixels of distance
+	public float ProjectileSpeed = 100; //speed in pixels per second
+	
+	//Advanced Stats - these won't be relevant all of the time, but will be semi-frequently
+	public float CritChance = 0; //pure chance, so 1 is 100% and 0 is 0%
+	public float CritMult = 2; //what final damage will be multiplied by when the crit triggers
+	public float ShotSpread = 0; //angle, in degrees, from pure accuracy that shots can potentially spread
+	public float ShotSpreadFalloff = 0; //a falloff for spread. At 0, all shots will be evenly distributed around the potential range. Higher numbers tighten spread, lower numbers cause more shots to go wider
+
+	//Extra Stats - rarer, used in more specific use cases
+	public int ChainTargets = 0;
+	
+
+	public TowerStats()
+	{
+
+	}
+
+
+
 }
