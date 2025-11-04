e47c053981d74ff3d3d5fa52e2ce943af4ccbc79
BlakeRossi3 <Blake.rossi13@gmail.com>
2025-11-03 23:40:00 -0500

    merge

--- Scripts/Gameplay/Enemies/Enemy.cs
+++ Scripts/Gameplay/Enemies/Enemy.cs
@@ -30,6 +30,11 @@ public partial class Enemy : PathFollow2D
 	private float _slowTimer = 0f;
 	private bool _isSlowed = false;
 
+	//Damage indicator 
+	[Export] public PackedScene DamageIndicatorScene;
+
+	Color damageColor;
+>>>>>>> Stashed changes
 	public override void _Ready()
 	{
 		HP = MaxHp;
@@ -78,6 +83,39 @@ public partial class Enemy : PathFollow2D
 
 	}
 
+	public void TakeDamage(float dmg)
+	{
+		TakeDamage(dmg, DamageType.Normal);
+	}
+
+
+	private void ShowDamageIndicator(float dmg, DamageType type)
+	{
+		//Make Damage Type enum - set indicator color off damage type?
+		if (DamageIndicatorScene == null)
+		{
+			GD.PushWarning("Damage indicator failed due to scene being null. Took " + dmg + " damage.");
+			return;
+		}
+
+		if(type == DamageType.Posion)
+		{
+			damageColor = new Color(0.0f, 1.0f,0.0f);
+		}
+		else
+		{
+			damageColor = new Color(1.0f, 0.7f, 0.0f);
+		}
+			
+		var indicator = (DamageIndicator)DamageIndicatorScene.Instantiate();
+		GetTree().CurrentScene.AddChild(indicator);
+
+		
+		indicator.GlobalPosition = GlobalPosition + new Vector2(0, -20);
+		indicator.SetDamage(dmg, damageColor);
+
+
+	}
 	private void OnAttackTimeout()
 	{
 		EmitSignal(SignalName.EnemyAttacked, this, AttackDamage);
